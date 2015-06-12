ea94538fa37a36dbaba453555ec71accf8652b23
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-06-13 00:51:59 +0900

    Add get access_token flow

--- QiitaKit/src/QiitaKit.swift
+++ QiitaKit/src/QiitaKit.swift
@@ -7,32 +7,88 @@
 //
 
 import Foundation
+import UIKit
 import APIKit
 import BrightFutures
+import LoggingKit
+
+extension AccessToken {
+    
+    public enum Scope: String {
+        case ReadQiita = "read_qiita"
+        case WriteQiita = "write_qiita"
+        
+    }
+    
+}
 
 /**
 *  <#Description#>
 */
-public class QiitaKit {
+public class QiitaKit: API {
     
-    /// aaa
-    /// bbb
-    let api: API
+    let baseURL: String
     
-    public init(baseURL: String) {
-        self.api = API(baseURL: baseURL)
+    let clientId: String
+    let clientSecret: String
+    
+    private var callbackScheme: String?
+    private var oauthPromise: Promise<AccessToken>?
+    private var accessToken: AccessToken?
+    
+    public init(baseURL: String, clientId: String, clientSecret: String) {
+        self.baseURL = baseURL
+        
+        self.clientId = clientId
+        self.clientSecret = clientSecret
+        
+        NSURLProtocol.registerClass(LoggingURLProtocol.self)
+        
+        super.init(baseURL: baseURL)
     }
     
-    public func request<T : RequestToken>(token: T) -> Future<T.Response> {
-        return self.api.request(token)
+    
+    public func oauthAuthorize(scopes: [AccessToken.Scope], scheme: String) -> Future<AccessToken> {
+        
+        let promise = Promise<AccessToken>()
+        
+        callbackScheme = scheme
+        oauthPromise = promise
+        
+        let app = UIApplication.sharedApplication()
+        let url = NSURL(string: "\(baseURL)/api/v2/oauth/authorize?client_id=\(clientId)&scope=read_qiita")
+        app.openURL(url!)
+        
+        return promise.future
     }
     
-    public func cancel<T : RequestToken>(clazz: T.Type) {
-        return self.api.cancel(clazz)
+    public func oauthCallback(url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        
+        if let scheme = callbackScheme,
+            let urlString = url.absoluteString,
+            let components = NSURLComponents(string: urlString),
+            let items = components.queryItems as? [NSURLQueryItem],
+            let code = items.filter({ $0.name == "code" }).first?.value
+            where urlString.hasPrefix(scheme)
+        {
+            let createAccessToken = CreateAccessToken(client_id: clientId, client_secret: clientSecret, code: code)
+            
+            let f = self.request(createAccessToken)
+            let p = oauthPromise!
+            f.onComplete {
+                p.complete($0)
+            }
+            return true
+        }
+        return false
     }
     
-    public func cancel<T : RequestToken>(clazz: T.Type, f: T -> Bool) {
-        return self.api.cancel(clazz, f: f)
-    }
+}
+
+class LoggingURLProtocol: NSURLProtocol {
     
+    override class func canInitWithRequest(request: NSURLRequest) -> Bool {
+        Logging.d(request)
+        return false
+    }
 }
